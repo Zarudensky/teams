f9e61a322695e4db639aab0c42af049847199b88
Stas Zarudensky <szarudensky@gmail.com>
2021-02-24 03:03:52 +0200

    add select search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayersComponent } from './players/players.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatIconModule,
+    NgxMatSelectSearchModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/demo-data.ts
@@ -0,0 +1,25 @@
+export interface Player {
+  id: string;
+  name: string;
+}
+
+export const PLAYERS: Player[] = [
+  {name: 'Stas', id: 'A'},
+  {name: 'Dima', id: 'B'},
+  {name: 'Alex', id: 'C'},
+  {name: 'Player D', id: 'D'},
+  {name: 'Player E', id: 'E'},
+  {name: 'Player F', id: 'F'},
+  {name: 'Player G', id: 'G'},
+  {name: 'Player H', id: 'H'},
+  {name: 'Player I', id: 'I'},
+  {name: 'Player J', id: 'J'},
+  {name: 'Player K', id: 'K'},
+  {name: 'Player L', id: 'L'},
+  {name: 'Player M', id: 'M'},
+  {name: 'Player N', id: 'N'},
+  {name: 'Player O', id: 'O'},
+  {name: 'Player P', id: 'P'},
+  {name: 'Player Q', id: 'Q'},
+  {name: 'Player R', id: 'R'}
+];
