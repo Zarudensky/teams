72e4acee11dcbee4be9dfff86986650e698e6f96
Stas Zarudensky <szarudensky@gmail.com>
2021-02-26 02:25:19 +0200

    Eddits style and settings for fierbase prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCA6D-uC_WJlk3tRkZDPIzuPGos-dyKBUY',
+    authDomain: 'teams-d98f6.firebaseapp.com',
+    databaseURL: 'https://teams-d98f6-default-rtdb.firebaseio.com',
+    projectId: 'teams-d98f6',
+    storageBucket: 'teams-d98f6.appspot.com',
+    messagingSenderId: '277595646270',
+    appId: '1:277595646270:web:3104ce915cb7a1cecce46f'
+  }
 };
